d65c392ac83f36830a452401c64c1a9c00aa33d2
Defiant7426 <pldelacruzv@gmail.com>
2025-08-27 20:35:05 -0500

    feat: Mostramos el razonamiento con veredicto final

--- frontend/app.py
+++ frontend/app.py
@@ -2,43 +2,60 @@ import streamlit as st
 import requests
 import json
 
-st.set_page_config(page_title="Agente Médigo", layout="centered")
+st.set_page_config(page_title="Agente Médigo", layout="wide")
 
 st.title("Analizador Médica con agentes de IA")
 
-st.info("MVP de un unico agente para clasificar la aptitud de un empleado en una empresa")
+st.info("Seleccciona un paciente y el Agente evaluará todas sus pruebas")
 
 paciente_id = st.selectbox(
-    "Seleccione un Paciente para Evaluar",
+    "Seleccione un Paciente para una evaluacion completa",
     ("P001", "P002", "P003", "P004"),
-    index = 0
+    index = 0,
+    help="El sistema buscará todas las pruebas necesarias para el perfil de este paciente"
 )
 
-prueba_a_evaluar = st.selectbox(
-    "Seleccione la Prueba",
-    ("Índice de Masa Corporal (IMC)", "Colesterol Total", "Edad")
-)
 
-if st.button(f"Evaluar '{prueba_a_evaluar}' para {paciente_id}"):
-    api_url = f"http://127.0.0.1:8000/evaluar/{paciente_id}"
-    params = {"prueba": prueba_a_evaluar}
+if st.button(f"Iniciar evaluación para {paciente_id}"):
+    api_url = f"http://127.0.0.1:8000/evaluar-perfil/{paciente_id}"
 
     with st.spinner(f"El agente esta analizando el paciente {paciente_id}..."):
         try:
-            response = requests.post(api_url,params=params)
+            response = requests.post(api_url)
 
             if response.status_code == 200:
                 data = response.json()
                 st.success("Evaluación Completada.")
 
-                conclusion = data.get('conclusion', 'N/A')
+                veredicto_general = data.get("veredicto_general", "N/A")
+                evaluaciones = data.get("evaluaciones", [])
 
-                if "No Apto" in conclusion:
-                    st.error(f"**Conclusion: {conclusion}**")
-                elif "Observado" in conclusion:
-                    st.warning(f"**Conclusion: {conclusion}**")
+                st.subheader("Veredicto General")
+
+                col1, col2 = st.columns([1, 4])
+
+                with col1:
+                    if "No Apto" in veredicto_general:
+                        st.error(f"**{veredicto_general}**")
+                    elif "Observado" in veredicto_general:
+                        st.warning(f"**{veredicto_general}**")
+                    else:
+                        st.success(f"**{veredicto_general}**")
+                st.subheader("Desglose de Resultado por prueba")
+
+                if not evaluaciones:
+                    st.warning("No se encontrarion evaluaciones individuales.")
                 else:
-                    st.success(f"**Conclusion: {conclusion}**")
+                    cols = st.columns(len(evaluaciones))
+                    for i, evaluacion in enumerate(evaluaciones):
+                        with cols[i]:
+                            # resultado = evaluacion.get("resultado", "N/A")
+                            st.metric(
+                                label = evaluacion.get("prueba", "Prueba Desconocida"),
+                                value= evaluacion.get("resultado", "N/A")
+                            )
+                            with st.expander("Ver razonamiento del agente"):
+                                st.text(evaluacion.get("razonamiento", "No disponible."))
                 
             else:
                 st.error(f"Error en la API: {response.status_code}")
